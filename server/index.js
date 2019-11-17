9b1674d82721006748f1ae404618a159f17f0884
Catalin Pit <catalinpit@gmail.com>
2019-11-17 09:14:35 +0000

    Added Subscriptions

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const Query = require('./resolvers/Query');
 const Mutation = require('./resolvers/Mutation');
 const User = require('./resolvers/User');
 const Link = require('./resolvers/Link');
+const Subscription = require('./resolvers/Subscription');
 
 //https://github.com/catalinpit/GraphQL-UM
 //https://www.howtographql.com/graphql-js/6-authentication/
@@ -12,6 +13,7 @@ const Link = require('./resolvers/Link');
 const resolvers = {
     Query,
     Mutation,
+    Subscription,
     User,
     Link
 };
